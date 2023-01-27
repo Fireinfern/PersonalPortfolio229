367befc508f8cbf571616ff63d379622ed60fe89
Sebastian Silva <sebastiansilvabenites@gmail.com>
2023-01-27 09:20:07 -0500

    Add Images and Logo

--- models/data/servicesData.js
+++ models/data/servicesData.js
@@ -1,13 +1,8 @@
 const Service = require("../service");
 
 module.exports = [
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
-    new Service("webDevelopment", "Web Development", "Develop web application with a functional backend service"),
+    new Service("webDevelopment", "Web Application Development", "Develop web application with a functional backend service, using express, hapi or Spring Boot, and a Front end developed in Angular, React or vanilla HTML, css and Javascript.", "images/webDev.webp"),
+    new Service("unity2DGame", "Unity 2D Game", "Develop your own 2D game using Unity!", "images/Unity.webp"),
+    new Service("unrealDeveloper", "Unreal Engine Game", "I will help you create the pototype for your 3D game using Unreal Engine", "images/UE.jpg"),
+    new Service("aidTheTeam", "Help With an Existing Project", "I will enter your development team and help with features and doubts in Unreal Engine", "images/UEHelp.png"),
 ]
